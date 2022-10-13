be45b001202d4e2c5f5bce264d72f6b108d4f8d7
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2022-10-12 22:49:30 -0700

    fixed linting errors

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
+  appName: 'Udagram',
   apiHost: 'http://udagram-api-dev2.us-east-1.elasticbeanstalk.com'
   // apiHost: 'http://localhost:8080/api/v0'
 };
